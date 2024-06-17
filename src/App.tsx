e898e4488fffcab3453f0650ceabc701cddd983d
ClearMist <cringleysdays@outlook.com>
2024-06-17 14:22:08 +0100

    feat: stock image for clarification; positioning of images & text

--- src/App.tsx
+++ src/App.tsx
@@ -24,11 +24,33 @@ export function App() {
                 Recipe Outlet
             </div>
             
-            <div>
+            <div className="flex items-center justify-center">
                 <p>
                     Discover, share and create authentic recipes with Recipe Outlet, a delightful 
                     experience watiting to be discovered.
                 </p>
+            </div>
+
+            <div className="flex items-center justify-center w-full h-auto">
+                <img className="w-32" src="src/assets/Stock-Image.jpg" alt="Stock"/>
+            </div>
+
+            <div>
+                <div className="flex justify-center item-center">
+                    <img className="w-32 h-auto" src="src/assets/Stock-Image.jpg" alt="Stock"/>
+                    <p className="w-1/3 py-10">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum nobis praesentium nesciunt fugiat cupiditate modi sapiente at ratione. Vitae mollitia aliquid officia consectetur adipisci rem sed voluptate repudiandae accusamus natus.
+                    </p>
+                </div>
+
+                <div className="flex items-center justify-center">
+                    <p className="w-1/3 py-10">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum nobis praesentium nesciunt fugiat cupiditate modi sapiente at ratione. Vitae mollitia aliquid officia consectetur adipisci rem sed voluptate repudiandae accusamus natus.
+                    </p>
+                    <img className="w-32 h-auto" src="src/assets/Stock-Image.jpg" alt="Stock"/>
+                </div>
+                
+                
             </div>
         
             <div className="flex items-center justify-center w-full bg-black gap-80">
